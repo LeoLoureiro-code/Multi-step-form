6f4a1fb1d3f5288aac7cfc1fd8916cdbcc5511db
Leo <leolou.90@gmail.com>
2026-07-05 22:50:52 -0700

    change name

--- src/components/go-back-button/go-back-button.ts
+++ src/components/go-back-button/go-back-button.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-go-back-button',
+  selector: 'go-back-button',
   imports: [],
   templateUrl: './go-back-button.html',
   styleUrl: './go-back-button.css',
