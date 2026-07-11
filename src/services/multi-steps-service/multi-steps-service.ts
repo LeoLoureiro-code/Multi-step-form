54b4018ed3d13f96a0f040e9ce55d17aabb4e43d
Leo <leolou.90@gmail.com>
2026-07-10 22:02:36 -0700

    add functions to move between steps

--- src/services/multi-steps-service/multi-steps-service.ts
+++ src/services/multi-steps-service/multi-steps-service.ts
@@ -5,4 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class MultiStepsService {
   
+  step:number = 1;
+
+  NextStep(){
+    if(this.step < 5){
+      this.step ++; 
+    }
+  }
+
+  PreviousStep(){
+    if(this.step >= 5){
+      this.step --;
+    }
+  }
+
 }
+
